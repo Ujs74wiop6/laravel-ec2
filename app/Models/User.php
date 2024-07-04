5cc1f0f091b15423eb615c2b82661e6c731d841a
Ubuntu <ubuntu@ip-172-31-29-131.ec2.internal>
2024-07-04 11:43:36 +0000

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,21 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
@@ -30,18 +37,33 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array<int, string>
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
+
+    public function events() {
+        return $this->hasMany('App\Models\Event');
+    }
+
+    public function eventsAsParticipant() {
+        return $this->belongsToMany('App\Models\Event');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,66 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+use App\Http\Controllers\EventController;
+
+/*
+Route::get('/', [EventController::class, 'index']);
+Route::get('/events/create', [EventController::class, 'create']);
+Route::get('/events/{id}', [EventController::class, 'show']);
+Route::post('/events', [EventController::class, 'store']);
+*/
+
+// Route::controller(EventController::class)->group(function () {
+//     Route::get('/', 'index');
+//     Route::get('/events/create', 'create')->middleware('auth');
+//     Route::get('/events/{id}', 'show');
+//     Route::post('/events', 'store');
+//     Route::delete('/events/{id}', 'destroy')->middleware('auth');
+//     Route::get('/events/edit/{id}', 'edit')->middleware('auth');
+//     Route::put('/events/update/{id}', 'update'->middleware('auth'));
+//     Route::get('/dashboard', 'dashboard')->middleware('auth');
+//     Route::get('/events/join/{id}')->middleware('auth');
+//     Route::get('/events/leave/{id}')->middleware('auth');
+// });
+
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
+
+Route::get('/', [EventController::class, 'index']);
+Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
+Route::get('/events/{id}', [EventController::class, 'show']);
+Route::post('/events', [EventController::class, 'store']);
+Route::delete('/events/{id}', [EventController::class, 'destroy'])->middleware('auth');
+Route::get('/events/edit/{id}', [EventController::class, 'edit'])->middleware('auth');
+Route::put('/events/update/{id}', [EventController::class, 'update'])->middleware('auth');
+
+Route::get('/contact', function () {
+    return view('contact');
 });
+
+Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
+
+Route::post('/events/join/{id}', [EventController::class, 'joinEvent'])->middleware('auth');
+
+Route::delete('/events/leave/{id}', [EventController::class, 'leaveEvent'])->middleware('auth');
